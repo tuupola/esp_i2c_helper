a42ad4c0dcdffb64e22af425c6d9d6c69b60913d
Mika Tuupola <tuupola@appelsiini.net>
2019-09-23 23:25:38 +0300

    Initial commit, untested code

---
+++ esp-i2c-hal.c
@@ -0,0 +1,114 @@
+/*
+
+Copyright (c) 2019 Mika Tuupola
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+#include <driver/i2c.h>
+#include <esp_log.h>
+#include <freertos/FreeRTOS.h>
+#include <stdint.h>
+
+#include "esp-i2c-hal.h"
+
+static const char* TAG = "esp-i2c-hal";
+
+static const uint8_t ACK_CHECK_ENABLE = 1;
+static const uint8_t ACK_CHECK_DISABLE = 0;
+
+i2c_hal_err_t i2c_hal_master_init() {
+    int i2c_master_port = I2C_HAL_MASTER_NUM;
+    ESP_LOGI(TAG, "Starting I2C master at port %d.", i2c_master_port);
+
+    i2c_config_t conf;
+    conf.mode = I2C_MODE_MASTER;
+    conf.sda_io_num = I2C_HAL_MASTER_SDA;
+    conf.sda_pullup_en = GPIO_PULLUP_DISABLE;
+    conf.scl_io_num = I2C_HAL_MASTER_SCL;
+    conf.scl_pullup_en = GPIO_PULLUP_DISABLE;
+    conf.master.clk_speed = I2C_HAL_MASTER_FREQ_HZ;
+
+    ESP_ERROR_CHECK(i2c_param_config(i2c_master_port, &conf));
+    ESP_ERROR_CHECK(
+        i2c_driver_install(
+            i2c_master_port,
+            conf.mode,
+            I2C_HAL_MASTER_RX_BUF_LEN,
+            I2C_HAL_MASTER_TX_BUF_LEN,
+            0
+        )
+    );
+
+    return ESP_OK;
+}
+
+i2c_hal_err_t i2c_hal_master_read(uint8_t address, uint8_t *buffer, uint16_t length) {
+    esp_err_t result;
+    i2c_cmd_handle_t command = i2c_cmd_link_create();
+
+    i2c_master_start(command);
+    i2c_master_write_byte(
+        command,
+        (address << 1) | I2C_MASTER_READ,
+        ACK_CHECK_ENABLE
+    );
+    if (length > 1) {
+        i2c_master_read(command, buffer, length - 1, I2C_MASTER_ACK);
+    }
+    i2c_master_read_byte(command, buffer + length - 1, I2C_MASTER_NACK);
+    i2c_master_stop(command);
+    result = i2c_master_cmd_begin(
+        I2C_HAL_MASTER_NUM,
+        command,
+        1000 / portTICK_RATE_MS
+    );
+    i2c_cmd_link_delete(command);
+
+    return result;
+}
+
+i2c_hal_err_t i2c_hal_master_write(uint8_t address, uint8_t *buffer, uint8_t size)
+{
+    esp_err_t result;
+    i2c_cmd_handle_t command = i2c_cmd_link_create();
+
+    i2c_master_start(command);
+    i2c_master_write_byte(
+        command,
+        (address << 1) | I2C_MASTER_WRITE,
+        ACK_CHECK_ENABLE
+    );
+    i2c_master_write(command, buffer, size, ACK_CHECK_ENABLE);
+    i2c_master_stop(command);
+    result = i2c_master_cmd_begin(
+        I2C_HAL_MASTER_NUM,
+        command,
+        1000 / portTICK_RATE_MS
+    );
+    i2c_cmd_link_delete(command);
+
+    return result;
+}
+
+i2c_hal_err_t i2c_hal_master_close() {
+    ESP_LOGI(TAG, "Closing I2C master at port %d.", I2C_HAL_MASTER_NUM);
+    return i2c_driver_delete();
+}
---
+++ esp-i2c-hal.h
@@ -0,0 +1,53 @@
+/*
+
+Copyright (c) 2019 Mika Tuupola
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+#ifndef _I2C_HAL_MASTER_H
+#define _I2C_HAL_MASTER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+typedef esp_err_t i2c_hal_err_t;
+
+#define I2C_HAL_MASTER_NUM          (1)
+#define I2C_HAL_MASTER_SDA          (22)
+#define I2C_HAL_MASTER_SCL          (23)
+#define I2C_HAL_MASTER_FREQ_HZ      (100000)
+#define I2C_HAL_MASTER_RX_BUF_LEN   (0)
+#define I2C_HAL_MASTER_TX_BUF_LEN   (0)
+
+
+i2c_hal_err_t i2c_hal_master_init();
+i2c_hal_err_t i2c_hal_master_read(uint8_t address, uint8_t *buffer, uint16_t size);
+i2c_hal_err_t i2c_hal_master_write(uint8_t address, uint8_t *buffer, uint16_t size);
+i2c_hal_err_t i2c_hal_master_close();
+//i2c_hal_err_t i2c_hal_master_ioctl(int16_t command, void *buffer);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
