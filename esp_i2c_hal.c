2f600730d301ed7d7b500b9fb33e026da243b5c8
Mika Tuupola <tuupola@appelsiini.net>
2020-04-03 00:47:36 +0700

    Silence discarded qualifiers warning

--- esp_i2c_hal.c
+++ esp_i2c_hal.c
@@ -113,7 +113,7 @@ int32_t i2c_hal_master_write(uint8_t address, uint8_t reg, const uint8_t *buffer
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, (address << 1) | I2C_MASTER_WRITE, ACK_CHECK_EN);
     i2c_master_write_byte(cmd, reg, ACK_CHECK_EN);
-    i2c_master_write(cmd, buffer, size, ACK_CHECK_EN);
+    i2c_master_write(cmd, (uint8_t *)buffer, size, ACK_CHECK_EN);
     i2c_master_stop(cmd);
     result = i2c_master_cmd_begin(I2C_HAL_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
     i2c_cmd_link_delete(cmd);
